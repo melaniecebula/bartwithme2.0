9d6b94b917eae915b31a77d8b28b1245d7e133c2
Melanie Cebula <melaniecebula@berkeley.edu>
2013-06-20 19:08:17 -0700

    socket.io, package.json, some basic mongoose

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //express
 var express = require('express')
   , http = require('http')
+  , app = express()
+  , server = http.createServer(app)
+  , sio = require('socket.io')
+  , io = sio.listen(server)
   , path = require('path');
-
-var app = express();
 
 //config express
 app.configure(function(){
@@ -23,6 +25,26 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+//mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    //yay!
+});
+
+var userSchema = mongoose.Schema({
+    name: String
+})
+
+var userInfoSchema = mongoose.Schema({
+    name: String
+    //other stuff
+})
+var userColl = mongoose.model('user', userSchema)
+var userInfoColl = mongoose.model('userinfo', userInfoSchema)
+
 //TODO:  check to see if user is logged in/ connected to fb already
 //redirect to '/'
 //TODO: sockets?  db?
@@ -30,7 +52,43 @@ app.get('/', function(req, res) {
     res.render("login");
 });
 
+app.get('/login', function(req, res) {
+    res.render("login")
+});
+
+//TODO post requests
+app.post('reportTimeAndRoute', function(req, res) {
+    var time = req.body.time;
+    var route = req.body.route;
+    var toInsert = {$set: {time: time, route: route}};
+});
+
 //listening on port 3000
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+//socket.io sockets
+//TODO:  understand sockets better :u
+io.sockets.on('connection', function (socket) {
+    socket.on('join', function (data) {
+        console.log(data);
+        var userID = data._id
+        var toInsert = {$set: {socketID: socket.id}};
+        //Model.update(conditions, update, [options], [callback])
+        userColl.update({socketID: socketID}, toInsert, function(err) {
+            if(err){
+                console.log(err);
+            }
+        });
+    });
+    socket.on('disconnect', function() {
+        var socketID = socket.id
+        var toInsert = {$set: {socketID: null}};
+        userColl.update({socketID:socketID}. toInsert, function(err) {
+            if(err){
+                console.log(err);
+            }
+        });
+    });
 });
