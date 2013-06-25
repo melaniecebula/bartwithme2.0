d8b50f2530cdb110372698ddec27bafd7c48f8c5
Melanie Cebula <melaniecebula@berkeley.edu>
2013-06-24 21:48:17 -0700

    bart shit

--- loginNodeServer/bart.js
+++ loginNodeServer/bart.js
@@ -8,39 +8,28 @@ var sys = require('util'),
 
 var parser = new xml2js.Parser();
 
-function getTime (location, destination, done) {
-    rest.get('http://www.bart.gov/dev/eta/bart_eta.xml').on('complete', function(result) {
+function getTimes (origin, destination, time) {
+    var query = {}
+    query.cmd = "depart"
+    query.orig = origin
+    query.dest = destination
+    query.time = time
+    query.date = "today"
+    query.key = "MW9S-E7SL-26DU-VV8V"
+    var options = {}
+    options.query = query
+    options.parser = rest.parsers.xml
+    rest.get('http://api.bart.gov/api/sched.aspx?', options).on('complete', function(result) {
       if (result instanceof Error) {
         sys.puts('Error: ' + result.message);
         this.retry(5000); // try again after 5 sec
       } else {
         //console.log(result)
         //return result
-        parser.parseString(result, function(err, data) {
-            //console.log("ALL STATIONS", data.root.station)
-            //console.log(station)
-            var stations = data.root.station
-            for (var i=0; i<stations.length; i++) {
-                if (stations[i]["name"] == location) {
-                    //console.log("you're at " + station +", and can leave for:")
-                    //console.log(data.root.station[i]["eta"])
-                    var destinations = stations[i]["eta"];
-                    console.log(destinations)
-                    for (var j=0; j<destinations.length; j++) {
-                        if (destinations[j]["destination"][0] == destination) {
-                            console.log(destinations[j]["estimate"]);
-                        };
-                    };
-                    console.log("finished")
-                    //console.log(destinations[0]["destination"][0] == destination);
-                    //done(destinations)
-                }
-            }
-        })}
-    });
+        console.log(result.root.schedule[0].request[0].trip)
+        }   
+   });
 }
 //example call with callback passed in
-getTime("Downtown Berkeley", "Fremont", function(result) {
-    console.log(result)
-});
+getTimes("ASHB", "CIVC", "10:00pm");
 
